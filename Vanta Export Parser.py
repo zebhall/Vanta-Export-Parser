fa41ef34da21b59525ae64afa18df536eb250ce2
zeb <zebhall@gmail.com>
2021-04-14 13:53:51 +0800

    formatting

--- Vanta Export Parser.py
+++ Vanta Export Parser.py
@@ -6,25 +6,29 @@ global headerOrder
 global headerIndexes
 global currentRowData
 global currentRowDataRaw
+global outputPrefix 
 #global rowCount
 headerOrder = ["Instrument Serial Num","Test Label","Date","Time","Method ID","Test Status","Real Time 1","Real Time 2","Real Time 3","User Factor Name","Units","Al Concentration","Al Error1s","As Concentration","As Error1s","Fe Concentration","Fe Error1s","Rb Concentration","Rb Error1s","Ti Concentration","Ti Error1s","LE Concentration","LE Error1s"]
 headerIndexes = []
 currentRowData = []
 currentRowDataRaw = []
+outputPrefix = "FORMATTED {}"
 #rowCount = 0
 
 
 
 def getInput():                             # Gets user input and fills related variables accordingly
 
-    global exportName
-    exportName = input("Vanta Export file name (including extension, e.g. chemistry-800151-2021-03-17-12-22-21.csv):")
-    print("Formatting", exportName, "to output.csv")
+    global inputName
+    global outputName
+    inputName = input("Input the Vanta Export file name (including extension, e.g. chemistry-800151-2021-03-17-12-22-21.csv):")
+    outputName = outputPrefix.format(inputName)
+    print("Formatting", inputName, "to", outputName)
 
 
 def getColumnIndex(columnHeader):           # Gets the index number of the column named in the input
-    with open(exportName) as exportFile:
-        reader = csv.reader(exportFile)
+    with open(inputName) as inputFile:
+        reader = csv.reader(inputFile)
         for row in reader:
             for k,v in enumerate(row):
                 if v == columnHeader:
@@ -32,8 +36,8 @@ def getColumnIndex(columnHeader):           # Gets the index number of the colum
 
 
 #def getRowCount():                          #Defunct function, unused.
-#    with open(exportName) as exportFile:
-#        reader = csv.reader(exportFile)
+#    with open(inputName) as inputFile:
+#        reader = csv.reader(inputFile)
 #        rowCount = sum(1 for row in reader)
 
 
@@ -47,9 +51,9 @@ def getHeaderIndexes():
 
 def fillData():                                             # pulls data from file using header indexes then writes it to output.csv, one row at a time.
     with open('output.csv', mode='w', newline='') as outputFile:
-        with open(exportName) as exportFile:
+        with open(inputName) as inputFile:
             writer = csv.writer(outputFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            reader = csv.reader(exportFile)
+            reader = csv.reader(inputFile)
             writer.writerow(headerOrder)
             for row in reader:
                 for i in headerIndexes:
@@ -64,8 +68,8 @@ def fillData():                                             # pulls data from fi
 #    while r>(rowCount+1):
 #        currentRowData = []
 #        currentRowDataRaw = []
-#        with open(exportName) as exportFile:
-#            reader = csv.reader(exportFile)
+#        with open(inputName) as inputFile:
+#            reader = csv.reader(inputFile)
 
 
 
